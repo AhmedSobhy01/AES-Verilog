b9051a5998d262e82cde1be86108a4251240cc45
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-04-21 19:06:42 +0200

    :art: Formatted Code

--- AddRoundKey.v
+++ AddRoundKey.v
@@ -1,5 +1,27 @@
 module AddRoundKey(state, roundKey, newRoundKey);
-input [127:0] state,roundKey;
-output [127:0] newRoundKey;
-assign newRoundKey= state^roundKey; 
+    input [127:0] state, roundKey;
+    output [127:0] newRoundKey;
+
+    assign newRoundKey= state ^ roundKey; 
+endmodule
+
+module AddRoundKey_DUT();
+    reg [127:0] state, roundKey;
+    wire [127:0] newRoundKey;
+
+    AddRoundKey ark(state, roundKey, newRoundKey);
+
+    initial begin
+        state = 128'h_046681e5_e0cb199a_48f8d37a_2806264c;
+        roundKey = 128'h_a0fafe17_88542cb1_23a33939_2a6c7605;
+        #10
+        state = newRoundKey;
+        roundKey = 128'h_a0fafe17_88542cb1_23a33939_2a6c7605;
+    end
+
+    initial begin
+        $display("AddRoundKey_DUT");
+        $display("==================================");
+        $monitor("State = %h, Key = %h, New Key= %h", state, roundKey, newRoundKey);
+    end
 endmodule
--- SubBytes.v
+++ SubBytes.v
@@ -1,13 +1,32 @@
-module SubBytes(oriBytes,subBytes);
-input [127:0] oriBytes;//original input bytes
-output wire [127:0] subBytes; //corresponding sub_bytes 
+module SubBytes(oriBytes, subBytes);
+	input [127:0] oriBytes; // Original input bytes
+	output wire [127:0] subBytes; // Corresponding sub_bytes 
 
-genvar i;
-generate 
-for(i=7;i<128;i=i+8) begin : Loop
-	SubTable s(oriBytes[i -:8],subBytes[i -:8]);
+	genvar i;
+	generate 
+		for (i=7;i<128;i=i+8) begin: SubTableLoop
+			SubTable s(oriBytes[i -:8],subBytes[i -:8]);
+		end
+	endgenerate
+endmodule
+
+module SubBytes_DUT();
+	reg [127:0] in;
+	wire [127:0] out;
+
+	SubBytes sb(in, out);
+
+	initial begin
+		in = 128'h00102030_40506070_8090a0b0_c0d0e0f0;
+		#10
+		in = 128'h89d810e8_855ace68_2d1843d8_cb128fe4;
+		#10
+		in = 128'h4915598f_55e5d7a0_daca94fa_1f0a63f7;
 	end
-endgenerate
 
-
+	initial begin
+		$display("SubBytes_DUT");
+		$display("==================================");
+		$monitor("Input = %h, Output = %h", in, out);
+	end
 endmodule
