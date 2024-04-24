b4c64a388fe5e96f2f1a349c4a3eb9b61fb41bb1
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-04-24 23:21:41 +0200

    :memo: Modified regs in main module

--- AES.v
+++ AES.v
@@ -4,9 +4,8 @@ module AES #(parameter Nk = 4, parameter Nr = 10) (encryptedOutputReg, decrypted
     output [6:0] HEX1;
     output [6:0] HEX2;
 
-    output reg [127:0] encryptedOutputReg = 0;
-    output reg [127:0] decryptedOutputReg = 0;
-
+    output reg [127:0] encryptedOutputReg = 128'h00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00;
+    output reg [127:0] decryptedOutputReg = 128'h00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00;
 
     // Key
     wire [127:0] key = 128'h_000102030405060708090a0b0c0d0e0f;
@@ -23,7 +22,7 @@ module AES #(parameter Nk = 4, parameter Nr = 10) (encryptedOutputReg, decrypted
     wire [127:0] tempDecryptedOutput;
 	
     // Binary to BCD Logic
-    reg [7:0] bcdInput;
+    reg [7:0] bcdInput = 8'b_00000000;
     wire [11:0] bcdOutput;
 	Binary2BCD b2b(bcdInput, bcdOutput);
 
@@ -33,49 +32,47 @@ module AES #(parameter Nk = 4, parameter Nr = 10) (encryptedOutputReg, decrypted
     DisplayDecoder dd3(bcdOutput[11:8], HEX2);
 
     // Encrypt
-    reg AESEncryptEnable = 1;
+    reg AESEncryptEnable = 1'b1;
     AESEncrypt AESE(data, allKeys, tempEncryptedOutput, clk, AESEncryptEnable);
 
     // Decrypt
-    reg AESDecryptEnable = 0;
+    reg AESDecryptEnable = 1'b0;
     AESDecrypt AESD(tempEncryptedOutput, allKeys, tempDecryptedOutput, clk, AESDecryptEnable);
 
-    reg [5:0] count = 0;
+    reg [4:0] count = 0;
     always @(posedge clk) begin
         if (AESEncryptEnable == 1 || AESDecryptEnable == 1)
             count <= count + 1;
     end
 
-    always @(count)begin
+    always @(count) begin
         if (count < Nr + 1)
             bcdInput = tempEncryptedOutput[7:0];
         else if (count == Nr + 1) begin
             encryptedOutputReg = tempEncryptedOutput;
             bcdInput = tempEncryptedOutput[7:0];
-            AESEncryptEnable = 0;
-            AESDecryptEnable = 1;
+            AESEncryptEnable = 1'b0;
+            AESDecryptEnable = 1'b1;
         end
         else if (count < ((Nr + 1) * 2))
             bcdInput = tempDecryptedOutput[7:0];
         else if (count == ((Nr + 1) * 2)) begin
             decryptedOutputReg = tempDecryptedOutput;
             bcdInput = tempDecryptedOutput[7:0];
-            AESDecryptEnable = 0;
+            AESDecryptEnable = 1'b0;
         end
-
     end
-
 endmodule
 
 module AES_DUT();
-    reg clk;
+    reg clk = 1'b0;
     wire [127:0] encrypted;
     wire [127:0] decrypted;
-    wire [6:0]HEX0,HEX1,HEX2;
+    wire [6:0] HEX0, HEX1, HEX2;
 
     AES AES(encrypted, decrypted, HEX0, HEX1, HEX2, clk);
 
-    reg [5:0] count = 0;
+    reg [4:0] count = 5'b00000;
     initial begin
         clk = 0;
         forever begin
